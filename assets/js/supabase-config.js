1a77c4a37983be85328712ea79c20475132a278a
Marik0023 <70592085+Marik0023@users.noreply.github.com>
2026-02-15 09:09:30 +0400

    Create supabase-config.js

---
+++ assets/js/supabase-config.js
@@ -0,0 +1,3 @@
+// assets/js/supabase-config.js
+window.MBQ_SUPABASE_URL = "https://jvlaqkqiglvoenropplz.supabase.co";
+window.MBQ_SUPABASE_ANON_KEY = "sb_publishable_hi9v9mvPkN4zujmQjooZYQ_xi_coZj1";
